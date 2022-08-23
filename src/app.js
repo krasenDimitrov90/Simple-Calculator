6f52fc2188f3a40a1c4c44bcb30a936d290d6222
krasenDimitrov90 <krasendimitrov90@gmail.com>
2022-08-23 07:20:22 +0300

    make module for the calculator

--- src/app.js
+++ src/app.js
@@ -1,3 +1,6 @@
+import { calc } from './calculator-create.js'
+const calculator = calc
+
 const numbers = document.querySelectorAll('.number');
 const operands = document.querySelectorAll('.operand');
 const deleteBtn = document.querySelector('.delete');
@@ -10,13 +13,13 @@ const currentValueEl = document.querySelector('.current-value');
 Array.from(numbers)
     .forEach(btn => btn.addEventListener('click', (e) => {
         calculator.appendNumber(e.currentTarget.textContent);
-        calculator.updateDisplay();
+        calculator.updateDisplay(currentValueEl, previousValueEl);
     }))
 
 Array.from(operands)
     .forEach(operand => operand.addEventListener('click', (e) => {
         calculator.updatePreviousNumberAsText(e.currentTarget.textContent);
-        calculator.updateDisplay();
+        calculator.updateDisplay(currentValueEl, previousValueEl);
     }))
 
 calculateBtn.addEventListener('click', () => {
@@ -24,94 +27,17 @@ calculateBtn.addEventListener('click', () => {
 
     calculator.compute(calculator.operation);
     calculator.finalResult();
-    calculator.updateDisplay();
+    calculator.updateDisplay(currentValueEl, previousValueEl);
     calculator.allClear();
 })
 
 deleteBtn.addEventListener('click', () => {
     calculator.delete();
-    calculator.updateDisplay();
+    calculator.updateDisplay(currentValueEl, previousValueEl);
 })
 
 allClearBtn.addEventListener('click', () => {
     calculator.allClear();
-    calculator.updateDisplay();
+    calculator.updateDisplay(currentValueEl, previousValueEl);
 })
 
-const calculator = {
-    operation: null,
-    previousOperandAsText: '',
-    currentOperandAsText: '',
-    finalResult() {
-        calculator.currentOperandAsText = calculator.previousOperandAsText.toString();
-        calculator.previousOperandAsText = '';
-        calculator.operation = null;
-    },
-    delete() {
-        calculator.currentOperandAsText = calculator.currentOperandAsText.slice(0, -1);
-    },
-    allClear() {
-        calculator.currentOperandAsText = '';
-        calculator.previousOperandAsText = '';
-        calculator.operation = null;
-    },
-    appendNumber(number) {
-        if (calculator.currentOperandAsText === '' && number === '.') {
-            calculator.currentOperandAsText = '0.';
-            return;
-        }
-        if (calculator.currentOperandAsText.includes('.') && number === '.') return;
-        calculator.currentOperandAsText += number.toString();
-    },
-    updateDisplay() {
-        if (calculator.currentOperandAsText.includes('.')) {
-            let [beforeDecimal, afterDecimal] = calculator.currentOperandAsText.split('.');
-            currentValueEl.textContent = Number(beforeDecimal).toLocaleString('fr') + '.' + afterDecimal;
-        } else if (!calculator.currentOperandAsText) {
-            currentValueEl.textContent = '';
-        } else if (!calculator.currentOperandAsText.includes('.')) {
-            currentValueEl.textContent = Number(calculator.currentOperandAsText).toLocaleString('fr')
-        }
-        
-        if (calculator.previousOperandAsText && calculator.operation) {
-            previousValueEl.textContent = calculator.previousOperandAsText + ' ' + calculator.operation;
-        } else if (!calculator.previousOperandAsText || !calculator.operation) {
-            previousValueEl.textContent = '';
-        }
-    },
-    updatePreviousNumberAsText(operation) {
-
-        if (!calculator.operation) {
-            calculator.operation = operation;
-            calculator.previousOperandAsText = calculator.currentOperandAsText;
-            calculator.currentOperandAsText = '';
-            calculator.updateDisplay();
-        } else if (calculator.currentOperandAsText === '') {
-            calculator.operation = operation;
-            calculator.updateDisplay();
-        } else if (calculator.currentOperandAsText !== '') {
-            calculator.compute(calculator.operation);
-            calculator.operation = operation;
-        }
-    },
-    compute(operation) {
-
-        let result;
-        switch (operation) {
-            case '÷':
-                result = parseFloat(calculator.previousOperandAsText) / parseFloat(calculator.currentOperandAsText);
-                break;
-            case '*':
-                result = parseFloat(calculator.previousOperandAsText) * parseFloat(calculator.currentOperandAsText);
-                break;
-            case '+':
-                result = parseFloat(calculator.previousOperandAsText) + parseFloat(calculator.currentOperandAsText);
-                break;
-            case '-':
-                result = parseFloat(calculator.previousOperandAsText) - parseFloat(calculator.currentOperandAsText);
-                break;
-        }
-        calculator.previousOperandAsText = result.toString();
-        calculator.currentOperandAsText = '';
-    }
-}
